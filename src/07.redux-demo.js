5d09ba70f8ff04b4cb6afcd2f18cd904028d14c5
asakao <anisorpchia@gmail.com>
2024-08-11 21:50:00 +0800

    async

--- src/07.redux-demo.js
+++ src/07.redux-demo.js
@@ -1,9 +1,15 @@
 import { increment, decrement, addToNum } from "./store/modules/counterStore";
 import { useSelector, useDispatch } from "react-redux";
+import { fetchChannelList } from "./store/modules/channelStore";
+import { useEffect } from "react";
 
 function ReduxDemo() {
     const { count } = useSelector(state => state.counter)
+    const { channelList } = useSelector(state => state.channel)
     const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(fetchChannelList())
+    }, [dispatch])
     return (
         <div>
             <button onClick={() => dispatch(decrement())}>-</button>
@@ -11,6 +17,9 @@ function ReduxDemo() {
             <button onClick={() => dispatch(increment())}>+</button>
             <button onClick={() => dispatch(addToNum(10))}>+10</button>
             <button onClick={() => dispatch(addToNum(20))}>+20</button>
+            <ul>
+                {channelList.map(item => <li key={item.id}>{item.name}</li>)}
+            </ul>
         </div>
 
     )
